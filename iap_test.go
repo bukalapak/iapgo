73b6a5723ec4088b3cb5c1882ce4798ce578cc59
Muhammad Saiful Islam <muhammad@saiful.web.id>
2019-10-11 15:51:38 +0700

    Use http.DefaultTransport to forward HTTP requests

--- iap_test.go
+++ iap_test.go
@@ -2,8 +2,29 @@ package iapgo_test
 
 import (
 	"net/http"
+	"net/http/httptest"
+	"testing"
 
 	"github.com/saifulwebid/iapgo"
 )
 
 var _ http.RoundTripper = new(iapgo.Transport)
+
+func TestTransport_RoundTrip(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.RequestURI != "/" {
+			t.Fatal("request URI doesn't match")
+		}
+	}))
+
+	client := http.Client{
+		Transport: new(iapgo.Transport),
+	}
+
+	req, err := http.NewRequest("GET", svr.URL, nil)
+	if err != nil {
+		t.Fatal("error creating a request")
+	}
+
+	client.Do(req)
+}
