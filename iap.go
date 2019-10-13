c31267f753c3fe07593ae301329abad83a165019
Muhammad Saiful Islam <muhammad@saiful.web.id>
2019-10-13 22:41:56 +0700

    Add comments to exported type and method

--- iap.go
+++ iap.go
@@ -1,3 +1,6 @@
+// Package iapgo helps authenticating access to endpoints behind Google Cloud
+// Identity-Aware Proxy (IAP).  It provides a Transport which implements
+// http.RoundTripper.
 package iapgo
 
 import (
@@ -15,6 +18,8 @@ var credentialsFinder credentialsFinderFn = google.FindDefaultCredentials
 
 var errUninitialized = errors.New("iapgo: unitialized Transport")
 
+// Transport implements http.RoundTripper that can be used to access endpoints
+// behind Google Cloud Identity-Aware Proxy.
 type Transport struct {
 	oauthTransport *oauth2.Transport
 }
@@ -46,6 +51,9 @@ func newTransport(clientID string) (*Transport, error) {
 	return transport, nil
 }
 
+// RoundTrip authenticates an HTTP request using an ID token.  This ID token is
+// retrieved using two-legged authentication with a Google endpoint defined in
+// the service account key.
 func (t *Transport) RoundTrip(r *http.Request) (*http.Response, error) {
 	if t.oauthTransport == nil {
 		return nil, errUninitialized
